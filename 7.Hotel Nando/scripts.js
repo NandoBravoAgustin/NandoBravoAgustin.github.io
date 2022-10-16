bf86f09381f4a48159223cbdd5337b7133313b38
NandoBravoAgustin <112024459+NandoBravoAgustin@users.noreply.github.com>
2022-10-16 14:19:32 +0200

    delete 7.hotel nando

--- 7.Hotel Nando/scripts.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector(".menu");
-const ulMenu = document.querySelector(".ul-menu")
-const cerrar = document.querySelector(".cruz")
-menu.onclick = function() {
-	ulMenu.classList.add("activo")
-}
-cerrar.onclick = function(){
-	ulMenu.classList.remove("activo")
-}
